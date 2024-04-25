ff13720f6cccd8c7cbed1b06ed4cee8c20a4913e
lAmeR1 <lamer1@github.com>
2024-04-26 01:50:03 +0200

    initial

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    exports: {
+
+      }
+};
 
 export default nextConfig;
--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,131 @@
+"use client";
+
+import { v5 as uuidv5 } from "uuid";
+
 import Image from "next/image";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { Html5QrcodeScanner } from "html5-qrcode";
+
+const MY_NAMESPACE = "1b671a64-40d5-491e-99b0-da01ff1f3341";
 
 export default function Home() {
+  const [uuid, setUuid] = useState(MY_NAMESPACE);
+  const [pw, setPw] = useState(MY_NAMESPACE);
+  const [addr, setAddr] = useState(null);
+
+  const createUuid = (e) => {
+    setUuid(uuidv5(e.target.phrase.value, MY_NAMESPACE));
+    setPw(e.target.phrase.value, MY_NAMESPACE);
+    e.preventDefault();
+  };
+
+  useEffect(() => {
+  
+    function onScanSuccess(decodedText, decodedResult) {
+      // handle the scanned code as you like, for example:
+      console.log(`Code matched = ${decodedText}`, decodedResult);
+    }
+    
+    function onScanFailure(error) {
+      // handle scan failure, usually better to ignore and keep scanning.
+      // for example:
+      console.warn(`Code scan error = ${error}`);
+    }
+    
+    let html5QrcodeScanner = new Html5QrcodeScanner(
+      "reader",
+      { fps: 10, qrbox: {width: 250, height: 250} },
+      /* verbose= */ false);
+    html5QrcodeScanner.render(onScanSuccess, onScanFailure);
+  }, [])
+
+  useEffect(() => {
+    axios
+      .get(`https://kaspagames.org/api/wallets/${uuid}`)
+      .then((resp) => {
+        console.log("resp", resp.data.publicAddress);
+        setAddr(resp.data.publicAddress)
+      })
+      .catch(() => {
+        // no wallet yet... creating new
+        axios
+          .post(`https://kaspagames.org/api/wallets`, { uuid: uuid, password: pw }, {headers: {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS, HEAD',
+            'Access-Control-Request-Method': 'POST',
+            'Access-Control-Allow-Headers': 'Content-Type'
+          }})
+          .then((resp) => {
+            console.log("new wallet created.");
+            setAddr(resp.data.publicAddress)
+          })
+          .catch((r) => {console.log("Error occured.", r)})
+      });
+  }, [uuid]);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+    <main
+      className="flex min-h-screen flex-row items-center justify-center text-white"
+      style={{ fontFamily: "Rubik" }}
+    >
+      {uuid ? (
+        <>
+          <div className="w-3/5 flex flex-col gap-y-16 p-16 items-start h-full align-top">
+            <div className="text-6xl font-extrabold text-white">
+              KASPA FAUCET
+            </div>
+            <div className="text-4xl ms-10 font-bold">
+              GET 1 FREE <span className="text-[#ffffff] font-">$KAS</span> HERE
+            </div>
+            <div className="text-4xl ms-20 font-bold flex flex-row gap-6">
+              IN LIGHTSPEED{" "}
+              <Image
+                src="speed.svg"
+                width="50"
+                height="50"
+                style={{ color: "white" }}
+              />
+            </div>
+          </div>
+          <div className="w-2/5 flex flex-col items-center">
+            <div className="text-xl">Just scan your wallet address</div>
+            <div className="h-64 border-red-100 border-4 p-8 m-8">
+              QR-Code scanner here
+              <div id="reader" width="400px"></div>
+            </div>
+
+            <div className="flex flex-col justify-start">
+              <div className="">TX-ID: 81124.....12412F</div>
+              <div className="">Block: 81124.....12412F</div>
+              <div className="">Time taken: 1.49s</div>
+              <div className="">High score: 0.29s</div>
+            </div>
+          </div>
+        </>
+      ) : (
+        <form onSubmit={createUuid}>
+          <div className="flex flex-col">
+            <div className="text-white text-3xl">
+              Please enter your wallet phrase:
+            </div>
+            <input
+              type="text"
+              name="phrase"
+              id="phrase"
+              placeholder="Wallet phrase"
+              className="p-1.5 mt-3 text-black"
             />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+            <button
+              type="submit"
+              className="p-2 my-3 bg-emerald-300/80 border-2"
+            >
+              GO
+            </button>
+          </div>
+        </form>
+      )}
+      {addr && <div className="absolute bottom-4 left-4 text-sm text-white/50">Faucet address: <a href={`https://explorer.kaspa.org/addresses/${addr}`} target="_blank">{addr}</a></div>}
     </main>
   );
 }
