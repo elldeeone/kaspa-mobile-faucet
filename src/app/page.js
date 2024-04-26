1b344a14ffcb4c044f1453ec5f159834bcb3872e
lAmeR1 <lamer1@github.com>
2024-04-26 02:03:15 +0200

    fix

--- src/app/page.js
+++ src/app/page.js
@@ -28,13 +28,14 @@ export default function Home() {
     function onScanSuccess(decodedText, decodedResult) {
       // handle the scanned code as you like, for example:
       console.log(`Code matched = ${decodedText}`, decodedResult);
-      setMsg("hier")
+      setMsg(decodedText)
     }
     
     function onScanFailure(error) {
       // handle scan failure, usually better to ignore and keep scanning.
       // for example:
       console.warn(`Code scan error = ${error}`);
+      setMsg(`Code scan error = ${error}`)
     }
     
     let html5QrcodeScanner = new Html5QrcodeScanner(
